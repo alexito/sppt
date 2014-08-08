47797106da578df3036dac69c9f3aff8df87ac72
Alexis Saransig <asaransig@metrodigi.com>
2014-08-08 00:17:59 -0500

    user improvements

--- src/java/org/controllers/UsuarioBean.java
+++ src/java/org/controllers/UsuarioBean.java
@@ -9,22 +9,40 @@ import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import org.database.Insert;
 import org.database.Select;
+import org.database.Update;
 import org.models.Usuario;
+import org.primefaces.event.RowEditEvent;
 
 @ManagedBean
 @Dependent
 public class UsuarioBean {
+
   private List<Usuario> listaUsuarios;
   private Usuario usuario;
-  
-  public void saveUsuario() throws IOException, SQLException{
+
+  public void saveUsuario() throws IOException, SQLException {
     Insert.InsertUsuario(usuario);
     FacesContext context = FacesContext.getCurrentInstance();
     ExternalContext extContext = context.getExternalContext();
     extContext.redirect("usuarios.jspx");
   }
-  
-  
+
+  public void onRowEdit(RowEditEvent event) throws SQLException {
+    int a = 0;
+    int b = 0;
+    Usuario editedUsuario = (Usuario) event.getObject();
+    Update.UpdateUsuario(editedUsuario);
+    b = a;
+//        OQs aux=((OQs) event.getObject());
+//        Actualizar.Actualizar_costo(aux.getId_oq(),aux.getQuantity_To_Receive(),aux.getSupplier_Name(),aux.getUnit_Cost());
+//        FacesMessage msg = new FacesMessage("OQ Edited", ((OQs) event.getObject()).getDescription2());
+//        FacesContext.getCurrentInstance().addMessage(null, msg);
+  }
+
+  public void onRowCancel(RowEditEvent event) {
+
+  }
+
   public Usuario getUsuario() {
     return usuario;
   }
@@ -40,10 +58,10 @@ public class UsuarioBean {
   public void setListaUsuarios(List<Usuario> listaUsuarios) {
     this.listaUsuarios = listaUsuarios;
   }
-  
+
   public UsuarioBean() {
     usuario = new Usuario();
     listaUsuarios = Select.selectUsuarios();
   }
-  
+
 }
--- src/java/org/database/Update.java
+++ src/java/org/database/Update.java
@@ -5,184 +5,57 @@
 package org.database;
 
 import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import org.models.Usuario;
 
 /**
  *
  * @author User
  */
 public class Update {
+  
+  public static String UpdateUsuario(Usuario usuario) throws SQLException {
+    ConnectDB con = new ConnectDB();
+    String SQL = "UPDATE usuario SET nombre=?, apellido=?, cedula=?, clave=?, email=?, telefono=?, estado=?, rol=? WHERE id=?";
+    PreparedStatement psUpdate = con.getConnection().prepareStatement(SQL);
+    psUpdate.setString(1, usuario.getNombre());
+    psUpdate.setString(2, usuario.getApellido());
+    psUpdate.setString(3, usuario.getCedula());
+    psUpdate.setString(4, usuario.getClave());
+    psUpdate.setString(5, usuario.getEmail());
+    psUpdate.setString(6, usuario.getTelefono());
+    psUpdate.setBoolean(7, usuario.getEstado());
+    psUpdate.setString(8, usuario.getRol());
+    psUpdate.setInt(9, usuario.getId());
     
-    public static String Actualizar_Usuario(int id_Usuario, String Nombre, String Apellido, String Rol, String Cedula, String Password, String Email, String Telefono) {
-        String resultado;
-        ConnectDB conexion = new ConnectDB();
-        try {            
-            CallableStatement prcProcedimientoAlmacenado = conexion.getConnection().prepareCall("{ call p_actualizar_usuario (?,?,?,?,?,?,?,?) }");
-            prcProcedimientoAlmacenado.setString(1, Nombre);
-            prcProcedimientoAlmacenado.setString(2, Apellido);
-            prcProcedimientoAlmacenado.setString(3, Rol);
-            prcProcedimientoAlmacenado.setString(4, Cedula);
-            prcProcedimientoAlmacenado.setString(5, Password);
-            prcProcedimientoAlmacenado.setInt(6, id_Usuario);
-            prcProcedimientoAlmacenado.setString(7, Email);
-            prcProcedimientoAlmacenado.setString(8, Telefono);
-            prcProcedimientoAlmacenado.execute();
-            prcProcedimientoAlmacenado.close();
-            resultado = ("Se actualizo correctamente");
-        } catch (Exception ex) {
-            resultado = (ex.getMessage());
-        } finally {
-            if (conexion.getConnection() != null) {
-                try {
-                    conexion.getConnection().close();
-                } catch (Exception e) {
-                }
-            }
-        }
-        if (resultado.equals("La instrucción no devolvió un conjunto de resultados.")) {
-            resultado = ("USUARIO ACTULIZADO CORRECTAMENTE");
-        }
-        return resultado;
-    }
+    return RunSQL(con, psUpdate);
     
-    public static String Actualizar_Estado_Usuario(int id_Usuario, boolean estado) {
-        String resultado;
-        ConnectDB conexion = new ConnectDB();
+  }
+  
+  private static String RunSQL(ConnectDB con, PreparedStatement psUpdate) {
+    
+    try {
+      psUpdate.executeUpdate();
+      con.getConnection().commit();
+      return "Los datos se guardaron Correctamente";
+    } catch (SQLException ex) {
+      return "No se pudo guardar los datos.";
+    } finally {
+      if (psUpdate != null) {
         try {
-             
-            CallableStatement prcProcedimientoAlmacenado = conexion.getConnection().prepareCall("{ call p_actualizar_estado_usuario (?,?) }");
-            prcProcedimientoAlmacenado.setInt(1, id_Usuario);
-            prcProcedimientoAlmacenado.setBoolean(2, estado);
-            prcProcedimientoAlmacenado.execute();
-            prcProcedimientoAlmacenado.close();
-            resultado = ("Se actualizo correctamente");
-        } catch (Exception ex) {
-            resultado = (ex.getMessage());
-        } finally {
-            if (conexion.getConnection() != null) {
-                try {
-                    conexion.getConnection().close();
-                } catch (Exception e) {
-                }
-            }
+          psUpdate.close();
+        } catch (SQLException e) {
+          return "Ocurrio un error inesperado";
         }
-        if (resultado.equals("La instrucción no devolvió un conjunto de resultados.")) {
-            resultado = ("USUARIO ACTULIZADO CORRECTAMENTE");
-        }
-        return resultado;
-    }
-    
-    public static String Actualizar_Password(int id_Usuario, String contraseña) {
-        String resultado;
-        ConnectDB conexion = new ConnectDB();
+      }
+      if (con.getConnection() != null) {
         try {
-             
-            CallableStatement prcProcedimientoAlmacenado = conexion.getConnection().prepareCall("{ call p_cambiar_password(?,?) }");
-            prcProcedimientoAlmacenado.setInt(1, id_Usuario);
-            prcProcedimientoAlmacenado.setString(2, contraseña);
-            prcProcedimientoAlmacenado.execute();
-            prcProcedimientoAlmacenado.close();
-            resultado = ("Se actualizo correctamente");
-        } catch (Exception ex) {
-            resultado = (ex.getMessage());
-        } finally {
-            if (conexion.getConnection() != null) {
-                try {
-                    conexion.getConnection().close();
-                } catch (Exception e) {
-                }
-            }
+          con.getConnection().close();
+        } catch (SQLException e) {
+          return "Ocurrio un error inesperado";
         }
-        if (resultado.equals("La instrucción no devolvió un conjunto de resultados.")) {
-            resultado = ("USUARIO ACTUALIZADO CORRECTAMENTE");
-        }
-        return resultado;
-    }
-    
-    public static String Actualizar_costo(int id_oq, float cantidad, String supplier, float costo) {
-        String resultado;
-        ConnectDB conexion = new ConnectDB();
-        try {
-             
-            CallableStatement prcProcedimientoAlmacenado = conexion.getConnection().prepareCall("{ call p_actualizar_cost(?,?,?,?) }");
-            prcProcedimientoAlmacenado.setInt(1, id_oq);
-            prcProcedimientoAlmacenado.setFloat(2, cantidad);
-            prcProcedimientoAlmacenado.setString(3, supplier);
-            prcProcedimientoAlmacenado.setFloat(4, costo);
-            prcProcedimientoAlmacenado.execute();
-            prcProcedimientoAlmacenado.close();
-            resultado = ("Se actualizo correctamente");
-        } catch (Exception ex) {
-            resultado = (ex.getMessage());
-        } finally {
-            if (conexion.getConnection() != null) {
-                try {
-                    conexion.getConnection().close();
-                } catch (Exception e) {
-                }
-            }
-        }
-        if (resultado.equals("La instrucción no devolvió un conjunto de resultados.")) {
-            resultado = ("USUARIO ACTUALIZADO CORRECTAMENTE");
-        }
-        return resultado;
-    }
-    
-    
-     public static String Actualizar_Estados(int id_estado, String descripcion, String descripcion_larga) {
-        String resultado;
-        ConnectDB conexion = new ConnectDB();
-        try {
-             
-            CallableStatement prcProcedimientoAlmacenado = conexion.getConnection().prepareCall("{ call p_actualizar_estados (?,?,?) }");
-            prcProcedimientoAlmacenado.setInt(1, id_estado);
-            prcProcedimientoAlmacenado.setString(2, descripcion);
-            prcProcedimientoAlmacenado.setString(3, descripcion_larga);
-            prcProcedimientoAlmacenado.execute();
-            prcProcedimientoAlmacenado.close();
-            resultado = ("Se actualizo correctamente");
-        } catch (Exception ex) {
-            resultado = (ex.getMessage());
-        } finally {
-            if (conexion.getConnection() != null) {
-                try {
-                    conexion.getConnection().close();
-                } catch (Exception e) {
-                }
-            }
-        }
-        if (resultado.equals("La instrucción no devolvió un conjunto de resultados.")) {
-            resultado = ("USUARIO ACTULIZADO CORRECTAMENTE");
-        }
-        return resultado;
-    }
-     
-     
-     
-     public static String Actualizar_Evento(String detalle, int estado, int id_evento) {
-        String resultado;
-        ConnectDB conexion = new ConnectDB();
-        try {
-             
-            CallableStatement prcProcedimientoAlmacenado = conexion.getConnection().prepareCall("{ call p_act_desactivar_evento (?,?,?) }");
-            prcProcedimientoAlmacenado.setInt(1, id_evento);
-            prcProcedimientoAlmacenado.setString(2, detalle);
-            prcProcedimientoAlmacenado.setInt(3, estado);
-            prcProcedimientoAlmacenado.execute();
-            prcProcedimientoAlmacenado.close();
-            resultado = ("Se actualizo correctamente");
-        } catch (Exception ex) {
-            resultado = (ex.getMessage());
-        } finally {
-            if (conexion.getConnection() != null) {
-                try {
-                    conexion.getConnection().close();
-                } catch (Exception e) {
-                }
-            }
-        }
-        if (resultado.equals("La instrucción no devolvió un conjunto de resultados.")) {
-            resultado = ("USUARIO ACTULIZADO CORRECTAMENTE");
-        }
-        return resultado;
-    }
+      }
+    }    
+  }
 }
