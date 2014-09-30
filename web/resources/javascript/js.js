d8b9b1359fc36f4ab01577ea33f1639e9d554b49
Alexis Saransig <asaransig@metrodigi.com>
2014-09-29 23:58:44 -0500

    style fix

--- web/resources/javascript/js.js
+++ web/resources/javascript/js.js
@@ -1,12 +1,9 @@
+
+//Controla los colores de las solicitudes, verde para un normal y rosado para una Emergencia
 setInterval(function(){
   $('.solicitud-tipo.Normal').parent().css('background-color', '#D1FFD1')
   $('.solicitud-tipo.Emergencia').parent().css('background-color', '#FFDADA')
 },2000);
-
-
-
-
-
 
 if (!checkRangeTime()) {
   $('.btn-nueva-solicitud').hide();
@@ -15,6 +12,8 @@ else {
   $('.btn-nueva-solicitud').show();
 }
 
+//Controla el boton de Nueva solicitud cada minuto. Verificando q el sistema esta operando 
+//dentro del horario permitido.
 setInterval(function() {
   if (!checkRangeTime()) {
     $('.btn-nueva-solicitud').hide();
